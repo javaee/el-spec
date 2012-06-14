67b3049cda44abc48d497d5ab1a4daaccfe3a997
Kin Man Chung <kinman.chung@oracle.com>
2012-06-14 22:18:20 +0000

    Lambda parameter cannot be null
    svn path=/trunk/; revision=117

--- api/src/main/java/javax/el/LambdaExpression.java
+++ api/src/main/java/javax/el/LambdaExpression.java
@@ -105,7 +105,8 @@ public class LambdaExpression {
      * removed after the evaluation.</p>
      *
      * @param elContext The ELContext used for the evaluation of the expression
-     * @param args The arguments for the Lambda expression
+     * @param args The arguments for the Lambda expression.  They cannot be
+     *    <code>null</code>
      * @return The result of invoking the Lambda expression
      * @throws ELException if not enough arguments are provided
      */
